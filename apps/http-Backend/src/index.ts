b1d6d95aac7fca195892984925f3aa5ef18ea2cf
anjeetsingh7155 <anjeetsingh7155>
2026-08-07 01:15:04 +0530

    Create : Created a signup endpoint in http-Backend

--- apps/http-Backend/src/index.ts
+++ apps/http-Backend/src/index.ts
@@ -1,12 +1,16 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
+import cors from 'cors'
+import { signup } from './signUpAndLogin-Route/signup';
 const app = express()
 
 const port = 5000
 
+app.use(cors())
+app.use(express.json())
 
-app.get("/" , (req : Request , res : Response)=>{
-res.send("Backend is working")
-})
+
+app.post("/signup",signup)
+
 
 app.listen(port, ()=>{
     console.log(`Http server is running on the http://localhost:${port}`)
