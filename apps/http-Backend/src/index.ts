812dfb3d19b51cd1529c9a223244f47c99d0caa2
anjeetsingh7155 <anjeetsingh7155>
2026-08-07 19:33:32 +0530

    Create : Created the skeleton of the crateRoom Route

--- apps/http-Backend/src/index.ts
+++ apps/http-Backend/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import { signup } from "./signUpAndLogin-Route/signup";
 import { login } from "./signUpAndLogin-Route/login";
+import { userMiddleWare } from "./MiddleWares/userMiddleWare";
+import { createRoom } from "./room-Routes/createRoom";
 const app = express();
 
 const port = 5000;
@@ -13,6 +15,9 @@ app.use(express.json());
 app.post("/v1/signup", signup);
 app.post("/v1/login", login);
 
+
+//Room Routes
+app.post('/v2/createRoom',userMiddleWare,createRoom)
 
 app.listen(port, () => {
   console.log(`Http server is running on the http://localhost:${port}`);
---
+++ apps/http-Backend/src/room-Routes/createRoom.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+
+export const createRoom = (res : Response,req : Request) =>{
+    //create a Db call
+res.json({
+    roomID : 123
+})
+}
